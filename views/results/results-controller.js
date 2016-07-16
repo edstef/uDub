a94b6a72a8ef8d0ec9f4d1f283f1e8ac599170f6
Joe Yang <jy22yang@uwaterloo.ca>
2016-07-16 08:40:29 -0700

    wip

--- views/results/results-controller.js
+++ views/results/results-controller.js
@@ -26,61 +26,7 @@
 				$('#id_loader').hide();
 				$('#result').show();
 			}, 100);
-			console.log(queryParams.get())
 			var params = queryParams.get();
-
-      $scope.result = {
-				"query": params.query,
-				"errors":[
-					{
-						"name": "Shit is broken",
-						"amount" : 19,
-						"questions": [
-							{
-								"title": "Error when using the thing",
-								"link" : "http://edwardmurakami.com/#/",
-								"votes" : 50,
-								"answered" : true
-							},
-							{
-								"title": "Why doesn't my thing work",
-								"link" : "http://edwardmurakami.com/#/",
-								"votes" : 513,
-								"answered" : false
-							},
-							{
-								"title": "Error when using the thing",
-								"link" : "http://edwardmurakami.com/#/",
-								"votes" : 240,
-								"answered" : false
-							},
-							{
-								"title": "Why doesn't my thing work",
-								"link" : "http://edwardmurakami.com/#/",
-								"votes" : 5150,
-								"answered" : true
-							}
-						],
-					}
-					// {
-					// 	"name": "Shit is super broken",
-					// 	"amount" : 19,
-					// 	"questions": [
-					// 		{
-					// 			"title": "Cant seem to access the thing",
-					// 			"link" : "http://edwardmurakami.com/#/",
-					// 			"votes" : 13,
-					// 			"answered" : true
-					// 		},
-					// 		{
-					// 			"title": "Factory factories no longer perform builders",
-					// 			"link" : "http://edwardmurakami.com/#/",
-					// 			"votes" : 230,
-					// 			"answered" : true
-					// 		}
-					// 	]
-					// }
-				]
 			};
 
 			$('#id_unanswered_checker').click(function(){
